22cf196ad1af043219df83514b77fc5f7a27e255
Marco Stroppel <marco.stroppel@gmail.com>
2024-04-21 11:22:30 +0200

    wip

--- AspNetCoreSample/AspNetCoreSample.Logic.Tests/WeatherForecastAutomoqTests.cs
+++ AspNetCoreSample/AspNetCoreSample.Logic.Tests/WeatherForecastAutomoqTests.cs
@@ -9,16 +9,5 @@ public class WeatherForecastAutomoqTests
     [Fact]
     public void GetForecast_ShouldReturnForecasts()
     {
-        // Arrange
-        var forecastStorageMock = new Mock<IForecastStorage>();
-        var forecastStorage = forecastStorageMock.Object;
-        var weatherForecastService = new WeatherForecastService(forecastStorage);
-
-        // Act
-        var forecasts = weatherForecastService.GetForecasts();
-
-        // Assert
-        forecasts.Should().NotBeNull();
-        forecasts.Should().HaveCount(5);
     }
 }
