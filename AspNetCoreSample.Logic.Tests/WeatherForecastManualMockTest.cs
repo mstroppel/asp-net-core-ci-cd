f1a73ea58dfd458fd398f0b46675973e5f0fab11
Marco Stroppel <marco.stroppel@gmail.com>
2024-04-22 14:54:39 +0200

    feat: add implementation and tests (#6)
    * wip
    
    * wip
    
    * add tests
    
    * add error
    
    * fix tests

--- AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
+++ AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
@@ -1,9 +1,35 @@
+using AspNetCoreSample.Outbound;
+
 namespace AspNetCoreSample.Logic.Tests;
 
 public class WeatherForecastManualMockTest
 {
     [Fact]
-    public void GetForecast_ShouldReturnForecasts()
+    public void GetForecast_ShouldReturnFiveForecasts()
     {
+        var systemUnderTest = new WeatherForecastService(new ForecastStorage());
+        
+        var forecasts = systemUnderTest.GetForecasts();
+        
+        Assert.NotNull(forecasts);
+        Assert.Equal(5, forecasts.Count());
+    }
+
+    [Fact]
+    public void GetForecasts_ShouldReturnCorrectTemperature()
+    {
+        var systemUnderTest = new WeatherForecastService(new ForecastStorage());
+        
+        var forecasts = systemUnderTest.GetForecasts();
+        
+        Assert.NotNull(forecasts);
+        Assert.Equal(20, forecasts.First().TemperatureC);
+    }
+    private class ForecastStorage : IForecastStorage
+    {
+        public OutboundWeatherForecast GetForecast(DateOnly date)
+        {
+            return new OutboundWeatherForecast(date, 20, "Sunny");
+        }
     }
 }
