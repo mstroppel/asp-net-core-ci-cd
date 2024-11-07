7342bfc098bed16868e3bc9be9f334dc85d34c90
Marco Stroppel <marco.stroppel@gmail.com>
2024-11-07 18:38:16 +0100

    Rollback for next presentation (#7)
    * Revert "fix tests"
    
    This reverts commit aad39bbe02715d6dc166a319ae8e48d94e23a249.
    
    * Revert "add error"
    
    This reverts commit ba9388dea3265b2a04cbeb49a5026cd91e4669a0.
    
    * Revert "add tests"
    
    This reverts commit ef8f2c424fad56c12c48c4bef8f538878a3e0ced.
    
    * Revert "wip"
    
    This reverts commit 573f3eed97fa894ce4f33d8220f7fae0375da9b6.

--- AspNetCoreSample.Logic.Tests/WeatherForecastAutomoqTests.cs
+++ AspNetCoreSample.Logic.Tests/WeatherForecastAutomoqTests.cs
@@ -9,16 +9,5 @@ public class WeatherForecastAutomoqTests
     [Fact]
     public void GetForecast_ShouldReturnForecasts()
     {
-        var forecastStorageMock = new Mock<IForecastStorage>();
-        forecastStorageMock
-            .Setup(x => x.GetForecast(It.IsAny<DateOnly>()))
-            .Returns(new OutboundWeatherForecast(DateOnly.FromDateTime(DateTime.Now), 20, "Sunny"));
-
-        var systemUnderTest = new WeatherForecastService(forecastStorageMock.Object);
-
-        var forecasts = systemUnderTest.GetForecasts();
-
-        forecasts.Should().NotBeNull();
-        forecasts.Should().HaveCount(5);
     }
 }
