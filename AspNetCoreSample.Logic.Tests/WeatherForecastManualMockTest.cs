aeaaa026b1c7cb401d5616af8224cbf44e9dd5d2
Marco Stroppel <marco.stroppel@gmail.com>
2024-11-07 18:28:05 +0100

    Revert "add tests"
    This reverts commit ef8f2c424fad56c12c48c4bef8f538878a3e0ced.

--- AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
+++ AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
@@ -1,35 +1,9 @@
-using AspNetCoreSample.Outbound;
-
 namespace AspNetCoreSample.Logic.Tests;
 
 public class WeatherForecastManualMockTest
 {
     [Fact]
-    public void GetForecast_ShouldReturnFiveForecasts()
+    public void GetForecast_ShouldReturnForecasts()
     {
-        var systemUnderTest = new WeatherForecastService(new ForecastStorage());
-        
-        var forecasts = systemUnderTest.GetForecasts();
-        
-        Assert.NotNull(forecasts);
-        Assert.Equal(6, forecasts.Count());
-    }
-
-    [Fact]
-    public void GetForecasts_ShouldReturnCorrectTemperature()
-    {
-        var systemUnderTest = new WeatherForecastService(new ForecastStorage());
-        
-        var forecasts = systemUnderTest.GetForecasts();
-        
-        Assert.NotNull(forecasts);
-        Assert.Equal(20, forecasts.First().TemperatureC);
-    }
-    private class ForecastStorage : IForecastStorage
-    {
-        public OutboundWeatherForecast GetForecast(DateOnly date)
-        {
-            return new OutboundWeatherForecast(date, 20, "Sunny");
-        }
     }
 }
