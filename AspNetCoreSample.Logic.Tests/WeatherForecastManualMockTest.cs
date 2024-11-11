9fc3ef1a7d1ea72d587ae1c4a004cf4a2981d606
Marco Stroppel <marco.stroppel@gmail.com>
2024-11-11 14:59:08 +0100

    implemented a test

--- AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
+++ AspNetCoreSample.Logic.Tests/WeatherForecastManualMockTest.cs
@@ -1,3 +1,5 @@
+using AspNetCoreSample.Outbound;
+
 namespace AspNetCoreSample.Logic.Tests;
 
 public class WeatherForecastManualMockTest
@@ -5,5 +7,26 @@ public class WeatherForecastManualMockTest
     [Fact]
     public void GetForecast_ShouldReturnForecasts()
     {
+        // Arrange
+        var forecastStorage = new MockForecastStorage();
+        var weatherForecastService = new WeatherForecastService(forecastStorage);
+
+        // Act
+        var forecasts = weatherForecastService.GetForecasts();
+
+        // Assert
+        Assert.Equal(5, forecasts.Count());
+    }
+
+    public class MockForecastStorage : IForecastStorage
+    {
+        public OutboundWeatherForecast GetForecast(DateOnly date)
+        {
+            return new OutboundWeatherForecast(
+                date,
+                25,
+                "Sunny"
+            );
+        }
     }
 }
